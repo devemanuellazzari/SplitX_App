c4c83c1448589b567d9ed84f43778297b3950fbb
Emanuel Ronan Lazzari <devemanuellazzari@gmail.com>
2026-01-02 09:54:16 -0300

    Tela de login
    Criada tela de login para inicio do projeto.

---
+++ SplitX.dpr
@@ -0,0 +1,14 @@
+program SplitX;
+
+uses
+  System.StartUpCopy,
+  FMX.Forms,
+  Frm_Principal in 'view\Frm_Principal.pas' {Form1};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.CreateForm(TForm1, Form1);
+  Application.Run;
+end.
